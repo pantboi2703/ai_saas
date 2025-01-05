5631ebed7899d02aa9e78ef3be97fa52ffc2a51f
Mayank <mayankpant257@gmail.com>
2025-01-05 17:37:41 +0530

    update

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ const DashboardLayout = async ({
             <div className="hidden h-full md:flex md:w-72 
             md:flex-col md:fixed md:inset-y-0
             bg-gray-900">
-                <Sidebar isPro={isPro} apiLimitCount={apiLimitCount}/>
+                <Sidebar isPro={isPro} apiLimitCounter={apiLimitCount}/>
             </div>
             <main className="md:pl-72">
                 <Navbar />
